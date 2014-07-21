bd61d3889a714045988985bc96f35024b2a59a4d
wolf <wbiker@gmx.at>
2014-07-21 18:35:38 +0200

    Add script to query DB and the DB itself

---
+++ db_query.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE: db_query.pl
+#
+#        USAGE: ./db_query.pl  
+#
+#  DESCRIPTION: 
+#   Provide commands to insert, update or select data in the sql db
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 21/07/14 17:50:54
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use feature qw(say);
+
+use DBI;
+
+my $dh = DBI->connect("dbi:SQLite:dbname='archeryLogger.sqlite'", "", "", { RaiseError => 1}) or die $DBI::errstr;
+
+say "Openening DB successfully!";
+
+
