43592511dc14d4c17fcb59e1e8241a29255a6982
wolf <wbiker@gmx.at>
2014-09-22 19:50:47 +0200

    pi feature enhanced with score_per_target value
    replaced the pi value with the new one.

--- set_sql.pl
+++ set_sql.pl
@@ -23,23 +23,22 @@ use warnings;
 use Data::Printer;
 use DBI;
 use Storable;
-__END__
+
 my $db = DBI->connect("dbi:SQLite:dbname=archeryLogger.sqlite", "", "");
 
-my $sessions = retrieve 'sessions';
+my $sth = $db->selectall_arrayref("SELECT * FROM archerysession", { Slice => {} });
+
+my $sessions = [];
+foreach my $session (@{$sth}) {
+    $session->{pi} = int(($session->{hit_targets} * $session->{score_per_target}) / $session->{levelid});
+    printf("sessionid: %s, hit targets: %s, PI: %s\n", $session->{sessionid}, $session->{hit_targets}, $session->{pi});
+    push($sessions, $session);
+}
+
 my $insert = $db->prepare("UPDATE archerysession set pi = ? WHERE sessionid = ?");
 foreach my $s (@$sessions) {
     say "sid: ".$s->{sessionid}." pi: ".$s->{pi};
     $insert->execute($s->{pi}, $s->{sessionid});
-}
-__END__
-my $sth = $db->selectall_arrayref("SELECT * FROM archerysession WHERE pi = '-1'", { Slice => {} });
-
-my $sessions = [];
-foreach my $session (@{$sth}) {
-    $session->{pi} = int(($session->{hit_targets} * 3) / $session->{levelid});
-    printf("sessionid: %s, hit targets: %s, PI: %s\n", $session->{sessionid}, $session->{hit_targets}, $session->{pi});
-    push($sessions, $session);
 }
 
 __END__
